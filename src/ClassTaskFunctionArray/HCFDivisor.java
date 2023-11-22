53754dc5fe313d55a746d8aca46363810bc06ee5
Adeshina Qudus <qudusa55@gmail.com>
2023-11-22 13:06:29 +0100

    another commit

---
+++ src/ClassTaskFunctionArray/HCFDivisor.java
@@ -0,0 +1,31 @@
+package ClassTaskFunctionArray;
+
+import java.util.ArrayList;
+
+public class HCFDivisor {
+
+    public static void main(String[] args) {
+        System.out.println(hcfDivisor(30,60));
+    }
+
+
+    public static ArrayList<Integer> hcfDivisor(int ...numbers){
+
+        ArrayList<Integer> divisorNumber = new ArrayList<>();
+        int counter = 0;
+        for (int count : numbers) {
+            for (int divisor = 2 ; divisor < 20; divisor++){
+                if (count % divisor == 0){
+                    counter++;
+                }
+                if (count % divisor != 0){
+                    divisor++;
+                }
+                if (counter == numbers.length){
+                    divisorNumber.add(count);
+                }
+            }
+        }
+        return divisorNumber;
+    }
+}
